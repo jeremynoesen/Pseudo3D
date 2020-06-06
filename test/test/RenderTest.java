76822068d8b49037a5c6f658d76a5092ea18218b
Jeremy Noesen <jeremynoesen@gmail.com>
2020-06-05 22:28:31 -0500

    Fixed sprite loader

--- test/test/RenderTest.java
+++ test/test/RenderTest.java
@@ -50,7 +50,7 @@ class TestPanel extends JPanel implements ActionListener {
     public TestPanel() {
         scene = new Scene();
         object = new Object();
-        object.setSprite(Sprites.get("BACKGROUND"));
+        object.setSprite(Sprites.get("front"));
         object.setGravity(0);
         object.setPosition(new Vector(0, 0, -40));
         scene.addObject(object);
@@ -72,18 +72,26 @@ class TestPanel extends JPanel implements ActionListener {
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (Keyboard.isPressed(KeyEvent.VK_W))
+        if (Keyboard.isPressed(KeyEvent.VK_W)) {
             object.setPosition(object.getPosition().setZ(object.getPosition().getZ() - 1));
-        if (Keyboard.isPressed(KeyEvent.VK_S))
+            object.setSprite(Sprites.get("front"));
+        }
+        if (Keyboard.isPressed(KeyEvent.VK_S)) {
             object.setPosition(object.getPosition().setZ(object.getPosition().getZ() + 1));
-        if (Keyboard.isPressed(KeyEvent.VK_A))
-            object.setPosition(object.getPosition().setX(object.getPosition().getX() - 5));
-        if (Keyboard.isPressed(KeyEvent.VK_D))
-            object.setPosition(object.getPosition().setX(object.getPosition().getX() + 5));
+            object.setSprite(Sprites.get("front"));
+        }
+        if (Keyboard.isPressed(KeyEvent.VK_A)) {
+            object.setPosition(object.getPosition().setX(object.getPosition().getX() - 1));
+            object.setSprite(Sprites.get("left"));
+        }
+        if (Keyboard.isPressed(KeyEvent.VK_D)) {
+            object.setPosition(object.getPosition().setX(object.getPosition().getX() + 1));
+            object.setSprite(Sprites.get("right"));
+        }
         if (Keyboard.isPressed(KeyEvent.VK_UP))
-            object.setPosition(object.getPosition().setY(object.getPosition().getY() + 5));
+            object.setPosition(object.getPosition().setY(object.getPosition().getY() + 1));
         if (Keyboard.isPressed(KeyEvent.VK_DOWN))
-            object.setPosition(object.getPosition().setY(object.getPosition().getY() - 5));
+            object.setPosition(object.getPosition().setY(object.getPosition().getY() - 1));
         scene.tick();
     }
 }
