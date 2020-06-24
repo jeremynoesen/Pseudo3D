a08bf27073ec3f0093e2604f7bcc718e3f1911db
Jeremaster101 <jeremynoesen@hotmail.com>
2020-06-24 16:35:44 -0500

    Made sprites load to path instead of file name, changed sprite load() to be able to load one file as well

---
+++ src/jndev/pseudo3d/loader/Sprite.java
@@ -0,0 +1,54 @@
+package jndev.pseudo3d.loader;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * this class is used to load and retrieve all sprites for a game
+ *
+ * @author JNDev (Jeremaster101)
+ */
+public class Sprite {
+    private static final HashMap<String, Image> sprites = new HashMap<>();
+    
+    /**
+     * this will load all sprite images into a hash map with the file's path as the key
+     *
+     * @param dir directory where sprites are stored
+     */
+    public static void load(File dir) {
+        if(dir.isDirectory()) {
+            for (File file : dir.listFiles()) {
+                if (file.isFile()) {
+                    try {
+                        sprites.put(file.getPath(), ImageIO.read(file));
+                    } catch (IOException e) {
+                        System.out.println("Failed to load sprite " + file.getPath());
+                    }
+                } else {
+                    if (file.isDirectory())
+                        load(file);
+                }
+            }
+        } else {
+            try {
+                sprites.put(dir.getPath(), ImageIO.read(dir));
+            } catch (IOException e) {
+                System.out.println("Failed to load sprite " + dir.getPath());
+            }
+        }
+    }
+    
+    /**
+     * get a sprite from the list of loaded sprites
+     *
+     * @param name name of sprite to retrieve
+     * @return Image object of sprite
+     */
+    public static Image get(String name) {
+        return sprites.get(name);
+    }
+}
--- test/test/Testing.java
+++ test/test/Testing.java
@@ -2,27 +2,28 @@ package test;
 
 import jndev.pseudo3d.game.Game;
 import jndev.pseudo3d.listener.Keyboard;
-import jndev.pseudo3d.loader.Sprites;
+import jndev.pseudo3d.loader.Sprite;
 import jndev.pseudo3d.object.Object;
 import jndev.pseudo3d.renderer.Camera;
 import jndev.pseudo3d.scene.Scene;
 import jndev.pseudo3d.util.Vector;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class Testing {
     
     public static void main(String[] args) {
-        Sprites.load(new File("res/sprites/"));
+        Sprite.load(new File("res/sprites/"));
         Scene scene = new Scene();
         Object object = new Object();
-        object.setSprite(Sprites.get("front"));
+        object.setSprite(Sprite.get("res/sprites/player/front.png"));
         object.setGravity(0);
         object.setPosition(new Vector(0, 0, 0));
-        object.setWidth(10);
-        object.setHeight(20);
-        object.setDepth(10);
+        object.setWidth(object.getSprite().getWidth(null));
+        object.setHeight(object.getSprite().getHeight(null));
+        object.setDepth(object.getSprite().getWidth(null));
         scene.addObject(object);
         
         for(int j = 1; j <= 20; j++) {
@@ -40,7 +41,7 @@ public class Testing {
         camera.setPosition(new Vector(0, 0, -100));
         
         scene.setCamera(camera);
-        //scene.setBackground(Color.BLACK);
+        scene.setBackground(Color.BLACK);
         
         Game.launch(1000, 1000);
         Game.getLoop().setActiveScene(scene);
@@ -50,14 +51,12 @@ public class Testing {
             if (Game.getLoop().getActiveScene().equals(scene)) {
                 if (Keyboard.isPressed(KeyEvent.VK_W)) {
                     object.setVelocity(object.getVelocity().setZ(-1));
-                    object.setSprite(Sprites.get("front"));
-                    object.setWidth(object.getSprite().getWidth(null));
+                    object.setSprite(Sprite.get("res/sprites/player/front.png"));
                 }
                 
                 if (Keyboard.isPressed(KeyEvent.VK_S)) {
                     object.setVelocity(object.getVelocity().setZ(1));
-                    object.setSprite(Sprites.get("front"));
-                    object.setWidth(object.getSprite().getWidth(null));
+                    object.setSprite(Sprite.get("res/sprites/player/front.png"));
                 }
                 
                 if (!Keyboard.isPressed(KeyEvent.VK_W) && !Keyboard.isPressed(KeyEvent.VK_S)) {
@@ -66,14 +65,12 @@ public class Testing {
                 
                 if (Keyboard.isPressed(KeyEvent.VK_A)) {
                     object.setVelocity(object.getVelocity().setX(-1));
-                    object.setSprite(Sprites.get("left"));
-                    object.setWidth(object.getSprite().getWidth(null));
+                    object.setSprite(Sprite.get("res/sprites/player/left.png"));
                 }
                 
                 if (Keyboard.isPressed(KeyEvent.VK_D)) {
                     object.setVelocity(object.getVelocity().setX(1));
-                    object.setSprite(Sprites.get("right"));
-                    object.setWidth(object.getSprite().getWidth(null));
+                    object.setSprite(Sprite.get("res/sprites/player/right.png"));
                 }
                 
                 if (!Keyboard.isPressed(KeyEvent.VK_A) && !Keyboard.isPressed(KeyEvent.VK_D)) {
@@ -99,8 +96,6 @@ public class Testing {
                 if (Keyboard.isPressed(KeyEvent.VK_DOWN)) {
                     camera.setFieldOfView(Math.max(camera.getFieldOfView() - 1, 0));
                 }
-                
-                System.out.println(object.getPosition());
             }
         });
     }
