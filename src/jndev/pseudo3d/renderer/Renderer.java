d0da43304753e506b172b58cb460feb1e511294c
Jeremy Noesen <jeremynoesen@gmail.com>
2020-06-25 10:00:37 -0500

    Refactoring and comments

--- src/jndev/pseudo3d/renderer/Renderer.java
+++ src/jndev/pseudo3d/renderer/Renderer.java
@@ -19,37 +19,37 @@ public class Renderer {
     /**
      * render a scene frame to a JPanel's graphics
      *
-     * @param scene scene to render
-     * @param panel jpanel to render to
+     * @param scene    scene to render
+     * @param panel    jpanel to render to
      * @param graphics jpanel graphics from paintComponent method
      */
     public static void render(Scene scene, JPanel panel, Graphics graphics) {
-        if(scene == null) return;
+        if (scene == null) return;
         //don't attempt rendering a null scene
         
         graphics.setColor(scene.getBackground());
         //draw scene background color
-    
+        
         graphics.fillRect(0, 0, panel.getWidth(), panel.getHeight());
         //clear out last drawn frame
-    
+        
         scene.getObjects().sort((o1, o2) -> (int) (o1.getPosition().getZ() - o2.getPosition().getZ()));
         //sort objects by z position so objects can be drawn in front of others
         
         for (int i = 0; i < scene.getObjects().size(); i++) {
             Object object = scene.getObjects().get(i);
-            
             Camera camera = scene.getCamera();
             Vector objPos = object.getPosition();
             Vector camPos = camera.getPosition();
             double fov = Math.toRadians(camera.getFieldOfView() / 2.0);
+            //commonly used values
             
-            if (object.getSprite() == null || camPos.getZ() - objPos.getZ() >= camera.getViewDistance())
-                continue; //don't render objects with no sprite or further than view distance
+            if (object.getSprite() == null || camPos.getZ() - objPos.getZ() >= camera.getViewDistance()) continue;
+            //don't render objects with no sprite or further than view distance
             
-            double scale = camera.getSensorSize() / (camera.getSensorSize() + (2 * (camPos.getZ() - objPos.getZ()) *
-                    (Math.sin(fov) / Math.sin((Math.PI / 2.0) - fov))));
-            //scale objects based on fov angle and distance from camera
+            double scale = camera.getSensorSize() / (camera.getSensorSize() +
+                    (2 * (camPos.getZ() - objPos.getZ()) * (Math.sin(fov) / Math.sin((Math.PI / 2.0) - fov))));
+            //scale objects based on fov angle and distance from camera using law of sines and camera sensor size
             
             if (Double.compare(scale, 0) == 0) continue;
             //do not render objects that are too small
@@ -70,7 +70,7 @@ public class Renderer {
             //boxes to represent image and panel bounds
             
             if (sprite.overlaps(screen)) {
-                //check if any part of image is within panel bounds
+                //check if any part of image is visible in panel
                 
                 graphics.drawImage(image, (int) (x - (widthScaled / 2.0)),
                         (int) ((panel.getHeight() - y) - (heightScaled / 2.0)),
