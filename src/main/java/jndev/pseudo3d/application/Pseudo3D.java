0985177ca44a36477560658a81a14cf7033e255c
Jeremy Noesen <jeremynoesen@gmail.com>
2020-12-01 14:18:23 -0600

    Convert Pseudo3D and GameLoop to JavaFX

--- src/main/java/jndev/pseudo3d/application/Pseudo3D.java
+++ src/main/java/jndev/pseudo3d/application/Pseudo3D.java
@@ -1,5 +1,10 @@
 package jndev.pseudo3d.application;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import jndev.pseudo3d.listener.Keyboard;
 import jndev.pseudo3d.listener.Mouse;
 
@@ -11,52 +16,75 @@ import java.awt.*;
  *
  * @author JNDev (Jeremaster101)
  */
-public class Pseudo3D extends JFrame {
-    
-    /**
-     * instance of application
-     */
-    private static final Pseudo3D pseudo3d = new Pseudo3D();
+public class Pseudo3D extends Application {
     
     /**
      * main game loop
      */
-    private static final GameLoop gameLoop = new GameLoop();
+    private GameLoop gameLoop;
     
     /**
-     * can't create a new instance outside Application class
+     * root pane of app
      */
-    private Pseudo3D() {
-    }
+    private Pane root;
     
     /**
-     * initialize the application. it is recommended that this is called as the first part of the main method
+     * canvas for root pane
+     */
+    private Canvas canvas;
+    
+    /**
+     * width of main window
+     */
+    private double width;
+    
+    /**
+     * height of main window
+     */
+    private double height;
+    
+    /**
+     * whether the window can be resized
+     */
+    private boolean resizable;
+    
+    /**
+     * title of window
+     */
+    private String title;
+    
+    /**
+     * create a new instance of the Pseudo3D engine with initial configuration
      *
-     * @param width     window width
-     * @param height    window height
-     * @param resizable whether the window can be resized
+     * @param width     width of window
+     * @param height    height of window
+     * @param resizable whether window can be resized
      * @param title     title of window
      */
-    public static void initialize(int width, int height, boolean resizable, String title) {
-        pseudo3d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pseudo3d.setSize(new Dimension(width, height));
-        pseudo3d.setLocationRelativeTo(null);
-        pseudo3d.setResizable(resizable);
-        pseudo3d.setTitle(title);
-        pseudo3d.add(gameLoop);
+    public Pseudo3D(double width, double height, boolean resizable, String title) {
+        gameLoop = new GameLoop(this);
+        root = new Pane();
+        canvas = new Canvas(width, height);
+        root.getChildren().add(canvas);
+        Mouse.initialize(root);
+        Keyboard.initialize(root);
+        this.width = width;
+        this.height = height;
+        this.resizable = resizable;
+        this.title = title;
     }
     
     /**
      * start the application
+     *
+     * @param primaryStage primary stage of application
      */
-    public static void launch() {
-        pseudo3d.setVisible(true);
-        gameLoop.start();
-        pseudo3d.requestFocus();
-        pseudo3d.addKeyListener(new Keyboard());
-        pseudo3d.addMouseListener(new Mouse());
-        pseudo3d.addMouseMotionListener(new Mouse());
-        pseudo3d.addMouseWheelListener(new Mouse());
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle(title);
+        primaryStage.setResizable(resizable);
+        primaryStage.setScene(new Scene(root, width, height));
+        primaryStage.show();
     }
     
     /**
@@ -69,11 +97,101 @@ public class Pseudo3D extends JFrame {
     }
     
     /**
-     * get the running instance of Pseudo3D
+     * change the game loop used for the engine
      *
-     * @return instance of Pseudo3D
+     * @param gameLoop gameloop to set as game loop
      */
-    public static Pseudo3D getInstance() {
-        return pseudo3d;
+    public void setGameLoop(GameLoop gameLoop) {
+        this.gameLoop = gameLoop;
+    }
+    
+    /**
+     * get the main canvas
+     *
+     * @return main canvas
+     */
+    public Canvas getCanvas() {
+        return canvas;
+    }
+    
+    /**
+     * get root pane
+     *
+     * @return root pane
+     */
+    public Pane getRoot() {
+        return root;
+    }
+    
+    /**
+     * set the width of the main window
+     *
+     * @param width width of main window
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    
+    /**
+     * get the width of the main window
+     *
+     * @return width of main window
+     */
+    public double getWidth() {
+        return width;
+    }
+    
+    /**
+     * set the height of the main window
+     *
+     * @param height height of main window
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    /**
+     * get the height of the main window
+     *
+     * @return height of main window
+     */
+    public double getHeight() {
+        return height;
+    }
+    
+    /**
+     * allow or disallow resizing the window
+     *
+     * @param resizable true to allow resizing
+     */
+    public void setResizable(boolean resizable) {
+        this.resizable = resizable;
+    }
+    
+    /**
+     * check if the window is resizable
+     *
+     * @return true if resizable
+     */
+    public boolean isResizable() {
+        return resizable;
+    }
+    
+    /**
+     * set the title of the window
+     *
+     * @param title window title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    /**
+     * get the title of the window
+     *
+     * @return window title
+     */
+    public String getTitle() {
+        return title;
     }
 }
