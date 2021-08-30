d97bc8b8fee98b5844ca53b4d17b0db7914ba6e9
Jeremy Noesen <jeremynoesen@gmail.com>
2021-08-29 19:49:43 -0500

    added missing author tags

--- src/main/java/xyz/jeremynoesen/pseudo3d/scene/util/Axis.java
+++ src/main/java/xyz/jeremynoesen/pseudo3d/scene/util/Axis.java
@@ -2,6 +2,8 @@ package xyz.jeremynoesen.pseudo3d.scene.util;
 
 /**
  * Axes of the grid
+ *
+ * @author Jeremy Noesen
  */
 public enum Axis {
     X, Y, Z;
--- src/main/java/xyz/jeremynoesen/pseudo3d/scene/util/Side.java
+++ src/main/java/xyz/jeremynoesen/pseudo3d/scene/util/Side.java
@@ -2,6 +2,8 @@ package xyz.jeremynoesen.pseudo3d.scene.util;
 
 /**
  * Sides of the Box
+ *
+ * @author Jeremy Noesen
  */
 public enum Side {
     LEFT, RIGHT, TOP, BOTTOM, BACK, FRONT;
